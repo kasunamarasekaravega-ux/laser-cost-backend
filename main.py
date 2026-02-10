8c209babaebf2b97df5501f81b7b655b7f61baed
Kasun Amarasekara <kasunamarasekara.vega@gmail.com>
2026-02-10 17:28:33 +0530

    Add CORS middleware

--- main.py
+++ main.py
@@ -29,7 +29,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # we tighten later
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
