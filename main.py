d367cf10d1136b2c1bf301ae2fc087ab0c43c587
Kasun Amarasekara <kasunamarasekara.vega@gmail.com>
2026-02-13 15:39:41 +0530

    Update CORS for new frontend domain

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://laser-cost-frontend-11fo.vercel.app",
+        "https://laser-cost-terminal.vercel.app",
     ],
     allow_credentials=False,
     allow_methods=["*"],
